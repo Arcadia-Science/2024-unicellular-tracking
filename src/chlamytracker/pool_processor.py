528b7d9b3f18c96a59351f4b86a39de080415f97
lanery <ryanilane@gmail.com>
2024-03-06 13:51:35 -0800

    cleanup: refactor and better documentation
    addresses multiple comments in #1

--- src/chlamytracker/pool_processor.py
+++ src/chlamytracker/pool_processor.py
@@ -7,14 +7,17 @@ from .utils import timeit
 
 
 class MicrochamberPoolProcessor:
-    """A 3D image stack representation of an agar microchamber pool.
+    """Class for processing timelapse microscopy data of an individual agar microchamber pool.
+
+    TODO: detailed description of what processing steps this class seeks to accomplish.
 
     Parameters
     ----------
-    stack : (Z, Y, X) array
-    r_median : scalar
+    stack : (T, Y, X) uint16 array
+        Input timelapse microscopy image data of an agar microchamber pool.
+    radius_median : scalar (optional)
         Radius of median filter to apply (preprocessing).
-    blur : scalar
+    sigma_gaussian : scalar (optional)
         Sigma of Gaussian filter for blurring the alpha mask (preprocessing).
 
     Attributes
@@ -23,35 +26,39 @@ class MicrochamberPoolProcessor:
     is_segmented : bool
     stack_raw : (Z, Y, X) array
         Raw (unprocessed) image stack.
-    stack_prp : (Z, Y, X) array
+    stack_preprocessed : (Z, Y, X) array
         Pre-processed image stack (prior to segmentation).
-    stack_seg : (Z, Y, X) array
-        Segmented image stack.
 
     Methods
     -------
     preprocess()
     segment()
+
+    Notes
+    -----
+    * Default values for `radius_median` and `sigma_gaussian` were chosen
+      empirically based on visual inspection of image data after preprocessing.
+
+    References
+    ----------
+    [1] https://doi.org/10.57844/arcadia-v1bg-6b60
     """
     def __init__(
         self,
         stack,
-        blur=4,
-        r_median=4,
+        radius_median=4,
+        sigma_gaussian=4,
     ):
 
         self.stack_raw = stack
-        self.blur = blur
-        self.r_median = r_median
+        self.radius_median = radius_median
+        self.sigma_gaussian = sigma_gaussian
 
         self.is_preprocessed = False
         self.is_segmented = False
 
     @timeit
-    def preprocess(
-        self,
-        remove_stationary_objects=True
-    ):
+    def preprocess(self, remove_stationary_objects=True):
         """Preprocess pool for cell tracking.
 
         Process
@@ -70,13 +77,13 @@ class MicrochamberPoolProcessor:
         Notes
         -----
         * Alpha mask is created by applying a Gaussian blur to a circle with
-          radius r = 1/2 width of prism.
+          radius r = 1/2 width of stack.
         """
 
         # apply median filter
         pool_filtered = median_filter_3d_parellel(
             self.stack_raw,
-            r_disk=self.r_median
+            r_disk=self.radius_median
         )
 
         # invert contrast
@@ -84,7 +91,7 @@ class MicrochamberPoolProcessor:
             pool_filtered
         )
 
-        # create alpha mask
+        # create alpha mask in the shape of a circle
         nz, ny, nx = self.stack_raw.shape
         mask = np.zeros((nz, ny, nx))
         rr, cc = ski.draw.disk(
@@ -95,21 +102,23 @@ class MicrochamberPoolProcessor:
         # apply Guassian blur to mask
         mask = ski.filters.gaussian(
             mask,
-            sigma=self.blur
+            sigma=self.sigma_gaussian
         )
-        # apply mask (prism --> tube)
+        # apply mask (transforms rectangular prism --> tube)
         pool_tube = mask * pool_inverted
 
         # estimate background from a central column of intensity values
-        z1, z2 = nz//2 - 50, nz//2 + 50
-        y1, y2 = round(ny//2 - 0.3*ny), round(ny//2 + 0.3*ny)
-        x1, x2 = round(nx//2 - 0.3*nx), round(nx//2 + 0.3*nx)
+        dz = 100  # column height
+        dy = round(60/100 * ny)  # column length = 60% total length of pool
+        dx = round(60/100 * nx)  # column width = 60% total width of pool
+        z1, z2 = nz//2 - dz//2, nz//2 + dz//2
+        y1, y2 = ny//2 - dy//2, ny//2 + dy//2
+        x1, x2 = nx//2 - dx//2, nx//2 + dx//2
         column = pool_tube[z1:z2, y1:y2, x1:x2]
-        bg = np.median(column)
-        # subtract background by clipping mean intensity of central column
+        # subtract background by clipping at median intensity of central column
         pool_rescaled = ski.exposure.rescale_intensity(
             pool_tube,
-            in_range=(bg, pool_tube.max()),
+            in_range=(np.median(column), pool_tube.max()),
             out_range=(0, 1)
         )
 
@@ -122,39 +131,45 @@ class MicrochamberPoolProcessor:
             )
 
         self.is_preprocessed = True
-        self.stack_prp = pool_rescaled
+        self.stack_preprocessed = pool_rescaled
 
     def segment(self):
         """Segment cells in preprocessed pool for tracking."""
 
         # if not self.is_preprocessed:
-        #     self.preprocess(remove_stationary_objects=True)
-
-        # self.is_segmented = True
-        # self.stack_seg = ...
+        #     self.preprocess()
 
         msg = "Robust segmentation is still in the works..."
         raise NotImplementedError(msg)
 
 
-def median_filter_3d_parellel(
-    stack,
-    r_disk=4,
-    n_workers=6
-):
+def median_filter_3d_parellel(stack, r_disk=4, n_workers=6):
     """Apply median filter to every image in a stack along the first axis
     (in parallel).
+
+    While it is unfortunately much slower, a median filter is used here
+    because it is much better than a mean or Gaussian filter at preserving
+    edges, which is desirable for cell detection and segmentation.
+
+    Parameters
+    ----------
+    r_disk : scalar
+        Radius of morphological footprint for median filter.
+    n_workers : int
+        Number of processors to dedicate for multiprocessing.
 
     Notes
     -----
     * Timing analysis showed diminishing returns beyond 6 workers.
     """
 
-    # make a bunch of footprints
+    # make a bunch of footprints for batch processing
     footprint = ski.morphology.disk(r_disk)
     footprints = [footprint]*stack.shape[0]
     # run median filter in parallel
-    with Pool(n_workers) as ws:
-        out = ws.starmap(ski.filters.median, zip(stack, footprints, strict=False))
+    with Pool(n_workers) as workers:
+        out = workers.starmap(
+            ski.filters.median, zip(stack, footprints, strict=False)
+        )
 
     return np.array(out)

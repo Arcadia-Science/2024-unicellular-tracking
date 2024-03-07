06865fa73291c972be7d0cf42ba8ad84c61060a2
lanery <ryanilane@gmail.com>
2024-03-06 17:45:37 -0800

    remove unused function

--- src/chlamytracker/utils.py
+++ src/chlamytracker/utils.py
@@ -1,6 +1,5 @@
 from functools import wraps
 from time import time
-import numpy as np
 
 
 def timeit(f):
@@ -15,18 +14,3 @@ def timeit(f):
         print(out)
         return result
     return wrap
-
-
-def setdiff2d(a, b):
-    """Finding the difference between two 2D arrays is surprisingly involved...
-    
-    References
-    ----------
-    [1] https://stackoverflow.com/a/11903368/5285918
-    """
-
-    a_ = a.view([('', a.dtype)] * a.shape[1])
-    b_ = b.view([('', b.dtype)] * b.shape[1])
-    c = np.setdiff1d(a_, b_).view(a.dtype).reshape(-1, a.shape[1])
-
-    return c

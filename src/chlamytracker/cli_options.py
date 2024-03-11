027ef51a468a7c11241615c0d39ddf36cdaad320
lanery <ryanilane@gmail.com>
2024-03-11 10:24:05 -0700

    more explicit argument handling for cli api

--- src/chlamytracker/cli_options.py
+++ src/chlamytracker/cli_options.py
@@ -2,20 +2,26 @@ from pathlib import Path
 
 import click
 
-data_dir_option = click.option("--data-dir", type=Path, help="Path to the data directory")
-data_file_option = click.option("--filename", type=Path, help="Path to the file location")
+data_dir_option = click.option(
+    "--data-dir", "data_dir", type=Path, help="Path to the data directory"
+)
+data_file_option = click.option(
+    "--filename", "filepath", type=Path, help="Path to the file location"
+)
 
 pool_radius_um_option = click.option(
     "--pool-radius",
+    "pool_radius_um",
     type=float,
     default=50,
     show_default=True,
-    help="Radius of agar microchamber pool.",
+    help="Radius [um] of agar microchamber pool.",
 )
 pool_spacing_um_option = click.option(
     "--pool-spacing",
+    "pool_spacing_um",
     type=float,
     default=200,
     show_default=True,
-    help="Distance between adjacent microchamber pools (measured from center to center).",
+    help="Distance [um] between adjacent microchamber pools (measured from center to center).",
 )

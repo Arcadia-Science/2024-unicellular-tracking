4e4b3e6f5e4eca563a4925ce346cf06facf4e0c6
lanery <ryanilane@gmail.com>
2024-03-13 15:06:16 -0700

    name change: more specific

--- src/chlamytracker/scripts/segment_cells_in_pools.py
+++ src/chlamytracker/scripts/segment_cells_in_pools.py
@@ -13,7 +13,8 @@ def main(data_dir, pool_radius_um, pool_spacing_um):
     """Wrapper for PoolFinder.extract_pools() and PoolFinder.export_pools()"""
 
     # glob all the nd2 files in directory
-    fps_nd2 = natsorted(data_dir.glob("*.nd2"))
+    # fps_nd2 = natsorted(data_dir.glob("*.nd2"))
+    fps_nd2 = natsorted(data_dir.glob("*_#[0-9]*.nd2"))
     if not fps_nd2:
         raise ValueError(f"No .nd2 files found in {data_dir}")
 

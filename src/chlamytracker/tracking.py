0721e7a6eefc95b9e2e00c6864ccde514bcb686f
lanery <ryanilane@gmail.com>
2024-03-13 15:06:46 -0700

    Create tracking.py
    First stab at tracking api.

---
+++ src/chlamytracker/tracking.py
@@ -0,0 +1,80 @@
+import btrack
+
+
+class PoolTracker:
+    """Class for tracking cell motility within agar microchamber pools.
+
+    Parameters
+    ----------
+    segmentation_data : (T, [Z], Y, X) uint8 numpy array
+        Segmented image data to input for cell tracking. Passed without
+        modification to `btrack.utils.segmentation_to_objects` [1]. Thus, other
+        forms of input are also accepted.
+    config_file : str or `pathlib.Path`
+        Filepath to configuration file for btrack parameters [2].
+    additional_properties : str or list-like
+        Additional object properties to record during tracking. Additional
+        properties are passed to `skimage.measure.regionprops` [3] via
+        `btrack.utils.segmentation_to_objects`.
+
+    References
+    ----------
+    [1] https://btrack.readthedocs.io/en/latest/api/btrack.io.segmentation_to_objects.html#btrack.io.segmentation_to_objects
+    [2] https://btrack.readthedocs.io/en/latest/user_guide/configuration.html
+    [3] https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.regionprops
+    """
+
+    def __init__(self, segmentation_data, config_file, additional_properties=None):
+        self.segmentation_data = segmentation_data
+        self.config_file = config_file
+        self.properties = self.set_properties(additional_properties)
+
+        self.trackable_objects = btrack.io.segmentation_to_objects(
+            segmentation_data, properties=self.properties
+        )
+
+    def set_properties(self, additional_properties):
+        """Handles additional object properties for tracking."""
+
+        base_properties = (
+            "area",
+            "eccentricity",
+            "major_axis_length",
+            "minor_axis_length",
+            "orientation",
+            "perimeter",
+            "solidity",
+        )
+
+        if additional_properties is None:
+            properties = base_properties
+        elif isinstance(additional_properties, str):
+            properties = base_properties + (additional_properties,)
+        elif isinstance(additional_properties, list) or isinstance(additional_properties, tuple):
+            properties = base_properties + tuple(additional_properties)
+        else:
+            msg = (
+                "Expected str or list (or tuple) of str for `additional_properties`, "
+                f"but received {type(additional_properties)}."
+            )
+            raise TypeError(msg)
+
+        return properties
+
+    def track_cells(self):
+        """Track cells.
+
+        Wrapper for `btrack.BayesianTracker().track()`.
+        """
+        self.tracker = btrack.BayesianTracker(verbose=True)
+        self.tracker.configure(self.config_file)
+        self.tracker.append(self.trackable_objects)
+        self.tracker.track()
+
+    # def export_tracks_to_csv(self, filename):
+    #     """"""
+    #     self.tracker
+
+    # def export_tracks_to_napari(self):
+    #     """"""
+    #     pass

2b94965be70a5655fdf01e3acdc265c2363c6c0f
Ryan Lane <ryan.lane@arcadiascience.com>
2024-04-04 14:02:07 -0700

    improve: reformat to use methods with parallel processing

--- src/chlamytracker/well_processor.py
+++ src/chlamytracker/well_processor.py
@@ -1,66 +1,67 @@
-import logging
-
 import numpy as np
-import skimage as ski
 
+from .stack_processing import (
+    gaussian_filter_3d_parallel,
+    otsu_threshold_3d,
+    remove_small_objects_3d_parallel,
+    rescale_to_float,
+)
 from .timelapse import Timelapse
 from .utils import timeit
 
-logger = logging.getLogger(__name__)
 
+class WellSegmenter(Timelapse):
+    """Subclass of `Timelapse` for segmenting timelapse microscopy data from one
+    well of a 384 or 1536 well plate."""
 
-class WellProcessor(Timelapse):
-    """Subclass for processing timelapse microscopy data from one well of a 384
-    or 1536 well plate."""
-
-    def __init__(self, nd2_file):
-        super().__init__(nd2_file)
-
-        self.is_zstack = self.dimensions.get("Z") is not None
+    def __init__(self, nd2_file, use_dask=False):
+        super().__init__(nd2_file, use_dask)
 
     @timeit
-    def segment(self, min_cell_area=64):
+    def segment(self, min_cell_diameter_um=6, filled_ratio_threshold=0.1):
         """"""
         # background subtraction
-        logger.info("Begin background subtraction...")
         background_subtracted = self.subtract_background()
-        logger.info("Background subtraction complete.")
 
         # different segmentation approaches depending on whether or not a zstack
         if self.is_zstack:
-            logger.info("Begin zstack timelapse segmentation...")
             segmentation = self._segment_zstack(background_subtracted)
         else:
-            logger.info("Begin timelapse segmentation...")
             segmentation = self._segment_zslice(background_subtracted)
-        logger.info("Segmentation complete.")
 
         # filter out small objects
-        logger.info(f"Removing objects smaller than {min_cell_area} px^2...")
-        segmentation_area_filterd = np.array(
-            [ski.morphology.remove_small_objects(im, min_size=min_cell_area) for im in segmentation]
+        min_area = self.convert_um_to_px2_circle(min_cell_diameter_um)
+        segmentation_area_filtered = remove_small_objects_3d_parallel(
+            segmentation, min_area=min_area, num_workers=10
         )
-        logger.info("Small object removal complete.")
 
-        return segmentation_area_filterd
+        # reject segmentations of noise
+        filled_ratio = segmentation_area_filtered.sum() / segmentation_area_filtered.size
+        if filled_ratio > filled_ratio_threshold:
+            msg = (
+                "Segmentation results are too noisy: segmented volume ratio "
+                f"{filled_ratio:.2f} above threshold "
+                f"{filled_ratio_threshold:.2f}."
+            )
+            raise ValueError(msg)
 
+        return segmentation_area_filtered
+
+    @timeit
     def subtract_background(self):
         """"""
         mean_projection = self.raw_data.mean(axis=0)
         background_subtracted = np.clip(self.raw_data - mean_projection, -np.inf, 0)
-        background_subtracted_rescaled = ski.exposure.rescale_intensity(
-            background_subtracted, out_range=(0, 1)
-        )
-        background_subtracted_inverted = 1 - background_subtracted_rescaled
-        background_subtracted_smoothed = ski.filters.gaussian(
-            background_subtracted_inverted, sigma=1.6
+        background_subtracted_rescaled = 1 - rescale_to_float(background_subtracted)
+        background_subtracted_smoothed = gaussian_filter_3d_parallel(
+            background_subtracted_rescaled, num_workers=10
         )
         return background_subtracted_smoothed
 
     def _segment_zslice(self, background_subtracted):
         """"""
-        thresh = ski.filters.threshold_otsu(background_subtracted)
-        segmentation = background_subtracted > thresh
+        threshold = otsu_threshold_3d(background_subtracted)
+        segmentation = background_subtracted > threshold
         return segmentation
 
     def _segment_zstack(self, background_subtracted):

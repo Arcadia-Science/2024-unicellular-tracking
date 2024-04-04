fc149c1b09d87e39c936b0944d17f21d63adc1f4
Ryan Lane <ryan.lane@arcadiascience.com>
2024-04-04 16:28:26 -0700

    improve: argument handling and logging

--- src/chlamytracker/scripts/track_cells_in_wells.py
+++ src/chlamytracker/scripts/track_cells_in_wells.py
@@ -1,5 +1,4 @@
 import logging
-from pathlib import Path
 
 import click
 import numpy as np
@@ -12,27 +11,23 @@ from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
-EXPORT_ROOT_DIRECTORY = Path("/home/theia/arc_nas_mirror/")
 
-
-def process_timelapse(filename, min_cell_diameter_um, btrack_config_file):
+def process_timelapse(
+    nd2_file,
+    output_directory,
+    use_dask,
+    min_cell_diameter_um,
+    btrack_config_file,
+):
     """Function for processing an individual file of raw timelapse microscopy
     data of unicellular organisms in agar microchamber pools."""
 
     # segmentation
-    well_processor = WellSegmenter(filename)
-    segmentation = well_processor.segment(min_cell_diameter_um)
-
-    # configure export directory
-    stripped_directory_tree = well_processor.nd2_file.parts
-    index_home = stripped_directory_tree.index("home")
-    stem_directory = Path(*stripped_directory_tree[index_home + 3 : -1]) / "processed"
-    export_directory = EXPORT_ROOT_DIRECTORY / stem_directory
-    export_directory.mkdir(parents=True, exist_ok=True)
-    print(export_directory)
+    well = WellSegmenter(nd2_file, use_dask=use_dask)
+    segmentation = well.segment(min_cell_diameter_um)
 
     # export segmentation
-    tiff_filename = export_directory / f"{well_processor.nd2_file.stem}_segmented.tiff"
+    tiff_filename = output_directory / f"{well.nd2_file.stem}_segmented.tiff"
     segmentation_8bit = (255 * segmentation).astype(np.uint8)
     ski.io.imsave(tiff_filename, segmentation_8bit)
 
@@ -41,43 +36,71 @@ def process_timelapse(filename, min_cell_diameter_um, btrack_config_file):
     well_tracker.track_cells()
 
     # export tracking data
-    csv_filename = export_directory / f"{well_processor.nd2_file.stem}_tracks.csv"
+    csv_filename = output_directory / f"{well.nd2_file.stem}_tracks.csv"
     well_tracker.tracker.export(csv_filename)
 
 
 @cli_api.btrack_config_file_option
 @cli_api.min_cell_diameter_um_option
+@cli_api.use_dask_option
 @cli_api.glob_option
-@cli_api.directory_argument
+@cli_api.output_directory_option
+@cli_api.input_directory_argument
 @click.command()
-def main(directory, glob_str, min_cell_diameter_um, btrack_config_file):
+def main(
+    input_directory,
+    output_directory,
+    glob_str,
+    use_dask,
+    min_cell_diameter_um,
+    btrack_config_file,
+):
     """Script for batch processing raw timelapse microscopy data of unicellular
     organisms in 384 or 1536 well plates.
 
-    For every nd2 file returned by the glob search pattern, this script will
-    perform segmentation and cell tracking and will output the results to
-    `{directory}/processed`. A tiff file is output for the segmentation and a
-    csv file of motility data is output for the cell tracking that contains the
-    (x, y) position and object properties (e.g. area, eccentricity, etc.) of
-    each tracked cell for each frame in the timelapse.
+    This script performs segmentation and cell tracking on each nd2 file
+    returned by the glob search pattern. Cell tracking will only proceed if
+    the segmentation was successful. If the segmentation fails (most likely
+    due to poor thresholding as a result of poor image quality or the absence
+    of cells), the nd2 file is skipped and nothing is output. If the
+    segmentation succeeds, a tiff file of the segmented timelapse is output and
+    cell tracking of the segmented timelapse will start. Cell tracking is done
+    using `btrack` [1]. Assuming cell tracking completes successfully, a csv
+    file of motility data is output that contains the (x, y) position and object
+    properties (e.g. area, eccentricity, etc.) of each tracked cell for each
+    frame in the timelapse.
+
+    Results are output to `{input_directory}/processed` by default if
+    `output_directory` is not specified.
+
+    References
+    ----------
+    [1] https://btrack.readthedocs.io/en/latest/index.html
     """
 
     # glob all .nd2 files in directory
-    nd2_files = natsorted(directory.glob(glob_str))
+    nd2_files = natsorted(input_directory.glob(glob_str))
     if not nd2_files:
-        raise ValueError(f"No nd2 files found in {directory}.")
+        raise ValueError(f"No nd2 files found in {input_directory}.")
+
+    # ensure output directory exists and is writeable
+    output_directory.mkdir(parents=True, exist_ok=True)
 
     # loop through nd2 files
     for nd2_file in tqdm(nd2_files):
         try:
-            process_timelapse(nd2_file, min_cell_diameter_um, btrack_config_file)
+            process_timelapse(
+                nd2_file,
+                output_directory,
+                use_dask,
+                min_cell_diameter_um,
+                btrack_config_file,
+            )
 
-        # skip over failures caused by
-        #   - corrupt nd2 files
-        #   - segmentation failures
+        # skip over segmentation failures and corrupt nd2 files
         except ValueError as err:
             msg = f"Processing for {nd2_file} failed:"
-            print(msg, err)
+            logger.warning(msg + str(err))
 
 
 if __name__ == "__main__":

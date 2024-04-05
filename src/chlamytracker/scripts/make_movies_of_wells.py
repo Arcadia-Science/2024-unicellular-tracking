5e236f598b3cee16dbffae528e62d6b7ffe8ffee
Ryan Lane <ryan.lane@arcadiascience.com>
2024-04-05 11:44:30 -0700

    integrate cli_api changes

--- src/chlamytracker/scripts/make_movies_of_wells.py
+++ src/chlamytracker/scripts/make_movies_of_wells.py
@@ -5,20 +5,24 @@ import napari
 import nd2
 import numpy as np
 import pandas as pd
-from chlamytracker import cli_options
+from chlamytracker import cli_api
 from chlamytracker.utils import crop_movie_to_content
 from napari_animation import Animation
 from natsort import natsorted
 from tqdm import tqdm
 
+logger = logging.getLogger(__name__)
+
 
 def make_napari_animation_for_timelapse(
     filename,
     nd2_file,
+    tiff_file,
     csv_file,
     framerate=20,
 ):
-    """Function for ...
+    """Renders a napari animation of tracked cells and overlays it onto the
+    raw timelapse data.
 
     Parameters
     ----------
@@ -26,8 +30,10 @@ def make_napari_animation_for_timelapse(
         Output filename for animation.
     nd2_file : Path
         Input timelapse microscopy data of tiny organisms swimming around in a well.
+    tiff_file : Path
+        Tiff file of segmented cells corresponding to the nd2 file.
     csv_file : Path
-        CSV file of motility data from a well.
+        Csv file of motility data corresponding to the nd2 file.
     """
     # load timelapse and metadata
     timelapse = nd2.imread(nd2_file)
@@ -36,7 +42,7 @@ def make_napari_animation_for_timelapse(
         num_frames = nd2f.sizes["T"]
 
     # create napari viewer
-    viewer = napari.Viewer(show=True)
+    viewer = napari.Viewer(show=False)
     viewer.add_image(timelapse[:, np.newaxis, :, :], name=nd2_file.stem)
 
     # load tracks (format: ID t x y z)
@@ -60,50 +66,62 @@ def make_napari_animation_for_timelapse(
     viewer.close()
 
 
-@cli_options.data_dir_option
+@cli_api.glob_option
+@cli_api.output_directory_option
+@cli_api.input_directory_argument
 @click.command()
-def main(data_dir, framerate=20):
+def main(
+    input_directory,
+    output_directory,
+    glob_str,
+    framerate=20,
+):
     """Script for batch processing napari animations of tracked cells.
 
     The following data files are needed to make a movie for each nd2 file
       - {timelapse}.nd2
+      - {timelapse}_segmented.tiff
       - {timelapse}_tracks.csv
+
+    Searches {input_directory} for __ and {output_directory} for the
+    corresponding tiff and csv files.
     """
 
     # glob all .nd2 files in directory
-    nd2_files = natsorted(data_dir.glob("*.nd2"))
+    nd2_files = natsorted(input_directory.glob(glob_str))
     if not nd2_files:
-        raise ValueError(f"No .nd2 files found in {data_dir}")
+        raise ValueError(f"No nd2 files found in {input_directory}.")
 
-    # loop through .nd2 files
+    # ensure output directory exists
+    if output_directory is None:
+        output_directory = input_directory / "processed"
+    if not output_directory.exists():
+        msg = f"Output directory: {output_directory} does not exist."
+        raise FileNotFoundError(msg)
+
+    # loop through nd2 files
     for nd2_file in tqdm(nd2_files):
-        # find csv file
-        csv_file = nd2_file.parent / "processed" / f"{nd2_file.stem}_tracks.csv"
 
-        # skip over timelapses with missing data
-        if not csv_file.exists():
-            msg = f"Processing for {nd2_file.name} failed: {csv_file} not found."
-            logging.error(msg)
+        # find tiff and csv files
+        tiff_file = output_directory / f"{nd2_file.stem}_segmented.tiff"
+        csv_file = output_directory / f"{nd2_file.stem}_tracks.csv"
+
+        # handle case for no tiff or csv file found
+        if not (tiff_file.exists() and csv_file.exists()):
+            logger.warning(f"No tiff or csv file corresponding to {nd2_file} found.")
             continue
 
-        # skip over corrupt nd2 files
-        try:
-            with nd2.ND2File(nd2_file) as nd2f:
-                _ = nd2f.shape
-        except ValueError as err:
-            msg = f"Processing for {nd2_file.name} failed:"
-            logging.error(msg, exc_info=err)
-            continue
-
-        # make napari animation
-        mp4_file = nd2_file.parent / f"{nd2_file.stem}.mp4"
+        # create napari animation
+        mp4_file = output_directory / f"{nd2_file.stem}_animation.mp4"
         make_napari_animation_for_timelapse(
             mp4_file,
             nd2_file,
+            tiff_file,
             csv_file,
+            framerate,
         )
 
-        # resize movie
+        # crop borders
         crop_movie_to_content(mp4_file, framerate)
 
 

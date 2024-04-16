891116149de237717c8ad176dc5aec45721ab3fe
Ryan Lane <ryan.lane@arcadiascience.com>
2024-04-16 12:31:37 -0700

    committing equations before I remove them all for conciseness

--- src/chlamytracker/tracking_metrics.py
+++ src/chlamytracker/tracking_metrics.py
@@ -12,15 +12,21 @@ class TrajectoryCSVParser:
     Parameters
     ----------
     csv_file : str or `pathlib.Path`
-        Path to csv file for analyzing.
+        Path to csv file of motility data for analysis.
+    frametime : float (optional)
+        Time increment [ms] between sequential rows of the motility data.
+        Equal to the exposure time of the timelapse or 1000 / frames per second.
     """
 
-    def __init__(self, csv_file):
-
-        # this is annoying but basically I output a ton of data with one particular
-        # csv format. there are a number of small things that distinguish them but
-        # the main one is that one is actually comma delimited whereas the other
-        # is white space delimited. so this is how I will differentiate them for now.
+    def __init__(self, csv_file, frametime=None):
+        # This is kind of awkward but basically the tracking data is output as
+        # a csv file in one of two styles. The first basically follows the btrack
+        # default plus a few extra columns for the properties such as area,
+        # eccentricity, etc. The second uses `Tracker.to_dataframe().to_csv()`.
+        # There are a few ways to differentiate between the two styles, but the
+        # main/easiest way is that only the second style is actually comma
+        # delimited. For whatever reason the btrack export outputs csvs as white
+        # space delimited.
         with open(csv_file) as csv:
             csv_headers = next(csv)
 
@@ -32,12 +38,13 @@ class TrajectoryCSVParser:
         else:
             raise ValueError(f"Unable to read csv file {csv_file}.")
 
-        self.trajectory_count = self.get_trajectory_count()
-        self.cell_count = self.estimate_cell_count()
+        if frametime is not None:
+            self.frametime = frametime
 
     def set_properties(self):
         """Handles additional object properties for tracking."""
 
+        # btrack.constants.DEFAULT_EXPORT_PROPERTIES
         default_btrack_csv_columns = [
             "ID",
             "t",
@@ -65,15 +72,35 @@ class TrajectoryCSVParser:
         return properties
 
     def get_trajectories(self):
-        """"""
+        """Get individual cell trajectories from motility data by ID number.
+
+        Returns
+        -------
+        cell_trajectories : (N, 2) float array
+            Cell trajectories as a series of (x, y) coordinates.
+        """
         cell_trajectories = []
         for _cell_id, cell_data in self.dataframe.groupby("ID"):
             x = cell_data["x"].values
             y = cell_data["y"].values
-            trajectory = TrajectoryAnalyzer(x, y)
+            trajectory = np.array([x, y]).T
             cell_trajectories.append(trajectory)
 
         return cell_trajectories
+
+    def measure_trajectories(self):
+        """
+
+        Returns
+        -------
+        dataframe : pd.DataFrame
+        """
+        if not hasattr(self, "frametime"):
+            msg = (
+                "Must initialize `TrajectoryCSVParser` with a frametime in "
+                "order to make trajectory measurements."
+            )
+            raise AttributeError(msg)
 
     def get_trajectory_count(self):
         """"""
@@ -95,92 +122,123 @@ class TrajectoryAnalyzer:
         X coordinates with respect to time.
     y : 1D float array
         Y coordinates with respect to time.
+    dt : float
+        Time increment [ms] between sequential (x, y) data points.
 
     References
     ----------
     [1]
     """
 
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, dt) -> None:
+        self.x = x
+        self.y = y
+        self.dt = dt
         self.points = np.array([x, y]).T
 
-        # # instantaneous velocities
-        # vx = np.diff(x)
-        # vy = np.diff(y)
+        # calculate instantaneous_quantities to facilitate motility measurements
+        self._calculate_instantaneous_quantities()
 
+        # motility measurements
+        t_tot = self.calculate_total_time()
+        d_tot = self.calculate_total_distance()
+        d_net = self.calculate_net_distance()
+        r_con = self.calculate_confinement_ratio()
+        v_crv = self.calculate_mean_curvilinear_speed()
+        v_lin = self.calculate_mean_linear_speed()
+        r_lin = self.calculate_linearity_of_progression()
+
+        self.measurements = {
+            "total_time": t_tot,
+            "total_distance": d_tot,
+            "net_distance": d_net,
+            "confinement_ratio": r_con,
+            "mean_curvilinear_speed": v_crv,
+            "mean_linear_speed": v_lin,
+            "linearity_of_progression": r_lin,
+        }
+
+    def _calculate_instantaneous_quantities(self):
+        """"""
         # instantaneous distances
-        self.distances_L1 = np.diff(self.points, axis=0)
-        self.distances_L2 = np.linalg.norm(self.distances_L1, axis=1)
+        self.L1_distances = np.diff(self.points, axis=0)
+        self.L2_distances = np.linalg.norm(self.L1_distances, axis=1)
 
-    #     # instantaneous angle
-    #     alpha = np.arctan2(vy, vx)
-    #     # instantaneous directional change
-    #     gamma = 3
+        # instantaneous angle
+        dx = np.diff(self.x)
+        dy = np.diff(self.y)
+        self.alpha = np.arctan2(dy, dx)
+        # instantaneous directional change
+        self.gamma = np.cumsum(np.abs(self.alpha))
 
-    #     self.total_distance_traveled = self.calculate_total_distance_traveled()
-    #     self.net_distance_traveled = self.calculate_net_distance_traveled()
-    #     self.confinement_ratio = self.calculate_confinement_ratio()
-    #     self.mean_curvilinear_speed = self.calculate_mean_curvilinear_speed()
-    #     self.mean_linear_speed = self.calculate_mean_linear_speed()
-    #     self.linearity_of_progression = self.calculate_linearity_of_progression()
-    #     self.mean_squared_displacement = self.calculate_mean_squared_displacement()
+        # instantaneous linear and angular velocity
+        self.linear_velocity = self.L2_distances / self.dt
+        self.angular_velocity = self.gamma / self.dt
 
-    # def calculate_total_distance_traveled(self):
-    #     """Calculate the total distance traveled by the cell.
+    def calculate_total_time(self):
+        """Calculate the total time in which the cell traveled.
 
-    #     Equation
-    #     --------
-    #     d_tot = sum[distance(p_i --> p_i+1)] from i = 0-->N
-    #     """
-    #     return self.distances_L2.sum()
+        Equation
+        --------
+        t_tot = N * dt
+        """
+        return self.x * self.dt
 
-    # def calculate_net_distance_traveled(self):
-    #     """Calculate the net distance traveled by the cell.
+    def calculate_total_distance(self):
+        """Calculate the total distance traveled by the cell.
 
-    #     Equation
-    #     --------
-    #     d_net = distance(p_0, p_N)
-    #     """
-    #     net_distance = np.linalg.norm(self.points[-1] - self.points[0])
-    #     return net_distance
+        Equation
+        --------
+        d_tot = sum( distance(p_i, p_i+1) ) for i = 0 --> N
+        """
+        return self.L2_distances.sum()
 
-    # def calculate_confinement_ratio(self):
-    #     """Calculate the confinement ratio.
+    def calculate_net_distance(self):
+        """Calculate the net distance traveled by the cell.
 
-    #     Equation
-    #     --------
-    #     r_con = d_net / d_tot
-    #     """
-    #     return self.net_distance_traveled / self.total_distance_traveled
+        Equation
+        --------
+        d_net = distance(p_0, p_N)
+        """
+        return np.linalg.norm(self.points[-1] - self.points[0])
 
-    # def calculate_instantaneous_angle(self):
-    #     """"""
-    #     pass
+    def calculate_confinement_ratio(self):
+        """Calculate the confinement ratio.
 
-    # def calculate_directional_change(self):
-    #     """"""
-    #     pass
+        Equation
+        --------
+        r_con = d_net / d_tot
+        """
+        return self.net_distance / self.total_distance
 
-    # def calculate_instantaneous_speed(self):
-    #     """"""
-    #     pass
+    def calculate_mean_curvilinear_speed(self):
+        """Calculate the average speed along the curved trajectory.
 
-    # def calculate_mean_curvilinear_speed(self):
-    #     """"""
-    #     pass
+        Equation
+        --------
+        v_avg = 1 / N * sum( v_i ) for i = 0 --> N
+        """
+        return self.linear_velocity.mean()
 
-    # def calculate_mean_linear_speed(self):
-    #     """"""
-    #     pass
+    def calculate_mean_linear_speed(self):
+        """Calculate the average straight line speed.
 
-    # def calculate_linearity_of_progression(self):
-    #     """"""
-    #     pass
+        Equation
+        --------
+        v_lin = d_net / t_tot
+        """
+        return self.net_distance / self.total_time
 
-    # def calculate_mean_squared_displacement(self):
-    #     """"""
-    #     pass
+    def calculate_linearity_of_progression(self):
+        """Calculate the linearity of forward progression.
 
-    # # def from_csv()
-    # # parse csv
-    # # return x, y
+        Equation
+        --------
+        r_lin = v_lin / v_avg
+        """
+        return self.mean_linear_speed / self.mean_curvilinear_speed
+
+    def to_dataframe(self):
+        """"""
+        dataframe = pd.DataFrame()
+        return dataframe

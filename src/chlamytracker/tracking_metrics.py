04dfb9a722f2f4c8e589fcf7375e4a6f1f2ccc9f
lanery <ryanilane@gmail.com>
2024-05-01 18:14:39 -0700

    Update tracking_metrics.py
    Refactored to include functionality for both scaled and unscaled trajectories.
    Tried to make consistent naming scheme for motility metrics.
    Added, modified, and removed motility metrics. still have concerns over curvature and discontinuity issue when calculating the change in direction.

--- src/chlamytracker/tracking_metrics.py
+++ src/chlamytracker/tracking_metrics.py
@@ -1,4 +1,5 @@
 import logging
+from math import pi
 
 import numpy as np
 import pandas as pd
@@ -11,7 +12,7 @@ class TrajectoryCSVParser:
 
     Parameters
     ----------
-    csv_file : str or `pathlib.Path`
+    csv_file : Path | str
         Path to csv file of motility data for analysis.
     framerate : float (optional)
         Frame rate [frames per second (fps)] of timelapse from which motility
@@ -82,12 +83,13 @@ class TrajectoryCSVParser:
         return properties
 
     def get_trajectories(self):
-        """Get individual cell trajectories from motility data by ID number.
+        """Get individual cell trajectories from motility data.
 
         Returns
         -------
-        cell_trajectories : (N, 2) float array
-            Cell trajectories as a series of (x, y) coordinates.
+        cell_trajectories : list
+            List of cell trajectories in the form of (x, y) coordinates stored
+            as (N, 2) numpy float arrays.
         """
         cell_trajectories = []
         for _cell_id, cell_data in self.dataframe.groupby("ID"):
@@ -98,22 +100,75 @@ class TrajectoryCSVParser:
 
         return cell_trajectories
 
-    def measure_trajectories(self):
-        """
+    def get_scaled_trajectories(self):
+        """Get individual cell trajectories from motility data.
 
         Returns
         -------
-        measurement_collection : list
-            List of trajectory measurements
+        cell_trajectories : list
+            List of cell trajectories in the form of (x, y) coordinates stored
+            as (N, 2) numpy float arrays.
         """
         if not hasattr(self, "framerate") or not hasattr(self, "pixelsize"):
             msg = (
                 "Must initialize `TrajectoryCSVParser` with framerate and "
-                "pixel size in order to make trajectory measurements."
+                "pixel size in order to get scaled trajectories."
             )
             raise AttributeError(msg)
 
-        measurement_collection = []
+        cell_trajectories = []
+        for _cell_id, cell_data in self.dataframe.groupby("ID"):
+            x = cell_data["x"].values
+            y = cell_data["y"].values
+            trajectory_analyzer = TrajectoryAnalyzer(
+                x, y, time_increment_s=self.frametime, scale_um=self.pixelsize
+            )
+            trajectory = trajectory_analyzer.xy_positions_um
+            cell_trajectories.append(trajectory)
+
+        return cell_trajectories
+
+    def compute_summary_statistics(self):
+        """Compute motility summary statistics for each trajectory in the motility data.
+
+        The set of summary statistics and how they are calculated are tabulated
+        in `TrajectoryAnalyzer`.
+
+        Returns
+        -------
+        measurement_collection : list
+            List of motility summary statistics.
+
+        Examples
+        --------
+        >>> from chlamytracker.tracking_metrics import TrajectoryCSVParser
+        >>> csv_file = "WellJ07_tracks.csv"
+        >>> motility_data = TrajectoryCSVParser(csv_file, framerate=30, pixelsize=0.5)
+        >>> motility_data.compute_summary_statistics()
+        >>> columns_subset = [
+                "cell_id",
+                "total_distance",
+                "confinement_ratio",
+                "max_sprint_length",
+            ]
+        >>> pd.DataFrame(summary_statistics)[columns_subset].head()
+
+        |   cell_id |   total_distance |   confinement_ratio |   max_sprint_length |
+        |----------:|-----------------:|--------------------:|--------------------:|
+        |         1 |          35.5695 |            0.997105 |            2.42239  |
+        |         3 |          45.0043 |            0.896259 |            1.36801  |
+        |         4 |          68.2631 |            0.959883 |            0.767712 |
+        |         5 |         323.361  |            0.687338 |            2.23714  |
+        |         7 |         180.866  |            0.812985 |            1.13697  |
+        """
+        if not hasattr(self, "framerate") or not hasattr(self, "pixelsize"):
+            msg = (
+                "Must initialize `TrajectoryCSVParser` with framerate and "
+                "pixel size in order to compute summary statistics."
+            )
+            raise AttributeError(msg)
+
+        summary_statistics = []
         for cell_id, cell_data in self.dataframe.groupby("ID"):
             x = cell_data["x"].values
             y = cell_data["y"].values
@@ -126,13 +181,13 @@ class TrajectoryCSVParser:
                 continue
 
             trajectory_analyzer = TrajectoryAnalyzer(
-                x, y, seconds=self.frametime, microns=self.pixelsize
+                x, y, time_increment_s=self.frametime, scale_um=self.pixelsize
             )
-            measurements = trajectory_analyzer.measurements
-            measurements["cell_id"] = cell_id
-            measurement_collection.append(measurements)
+            motility_metrics = trajectory_analyzer.compute_motility_metrics()
+            motility_metrics["cell_id"] = cell_id
+            summary_statistics.append(motility_metrics)
 
-        return measurement_collection
+        return summary_statistics
 
     def get_trajectory_count(self):
         """Get number of cell trajectories in motility data."""
@@ -141,32 +196,42 @@ class TrajectoryCSVParser:
     def estimate_cell_count(self):
         """Estimate the number of cells in the motility data.
 
-        In general this will be somewhat close but not equal to the trajectory
-        count since new trajectories are created each time cells collide or go
-        in and out of focus.
+        Estimate is based on the average number of trajectories in each frame.
+        In general this will not be equal to the trajectory count since new
+        trajectories are created each time cells collide or come into focus.
         """
-        num_cells = 3
+        cell_counts_per_frame = []
+        # count the number of trajectories within each frame
+        for _time, frame in self.dataframe.groupby("t"):
+            cell_count = len(frame)
+            cell_counts_per_frame.append(cell_count)
 
-        return num_cells
+        # number of cells should be ~= average cell count per frame
+        num_cells = np.mean(cell_counts_per_frame).round()
+        return int(num_cells)
 
 
 class TrajectoryAnalyzer:
-    """Class for analyzing cell trajectories.
+    """Class for analyzing cell trajectories and computing motility metrics.
 
-    Motility measurements adapted and expanded from [1].
+    Motility metrics adapted and expanded from [1].
     +--------------------------+----------------------------------+---------+
     | Variable                 | Equation                         | Unit    |
     +--------------------------+----------------------------------+---------+
     | total_time               | t_tot = N * dt                   | s       |
     | total_distance           | d_tot = sum( d(p_i, p_i+1) )     | µm      |
     | net_distance             | d_net = d(p_0, p_N)              | µm      |
-    | max_distance             | d_max = max( d(p_i, p_i+1) )     | µm      |
+    | max_sprint_length        | d_max = max( d(p_i, p_i+w) )     | µm      |
     | confinement_ratio        | r_con = d_net / d_tot            |         |
-    | mean_curvilinear_speed   | v_avg = 1 / N * sum( v_i )       | µm / s  |
+    +--------------------------+----------------------------------+---------+
+    | mean_curvilinear_speed   | v_avg = d_tot / t_tot            | µm / s  |
     | mean_linear_speed        | v_lin = d_net / t_tot            | µm / s  |
-    | mean_angular_velocity    | v_ang =                          | rad / s |
-    | linearity_of_progression | r_lin = v_lin / v_avg            |         |
+    | max_sprint_speed         | v_max = max( v(p_i, p_i+w) )     | µm / s  |
+    +--------------------------+----------------------------------+---------+
+    | mean_angular_velocity    | v_ang = r_tot / t_tot            | rad / s |
+    | num_rotations            | n_rot = r_tot / 2π               |         |
     | num_direction_changes    | n_chg = sum( sign(v_i+1 - v_i) ) |         |
+    | pivot_rate               | r_piv = n_chg / d_tot            | 1 / µm  |
     +--------------------------+----------------------------------+---------+
 
     Parameters
@@ -175,53 +240,81 @@ class TrajectoryAnalyzer:
         X coordinates with respect to time.
     y : 1D float array
         Y coordinates with respect to time.
-    seconds : float (optional)
+    time_increment_s : float (optional)
         Time increment [s] between sequential (x, y) data points.
-        Frame time of timelapse from which motility data originates.
-    microns : float (optional)
+        This should be equivalent to the frame time of timelapse from which
+        motility data originates.
+    scale_um : float (optional)
         Distance [µm] that a cell moves for each integer step in (x, y).
-        Pixel size [µm / px] of timelapse from which motility data originates
-        (assumes square pixels).
+        This should be equivalent to the pixel size [µm / px] of timelapse from
+        which motility data originates (assumes square pixels).
+    window_size : int (optional)
+        Window size for computing the metrics `max_sprint_length` and
+        `max_sprint_speed`.
 
     References
     ----------
     [1] https://doi.org/10.1016/B978-0-12-391857-4.00009-4
     """
 
-    def __init__(self, x, y, seconds=1, microns=1) -> None:
-        # create time axis based on number of points in trajectory
-        num_points = x.size
-        self.time_seconds = np.arange(num_points) * seconds  # t [s]
-        # calibrated trajectory
-        self.x_position_microns = x * microns  # x(t) [µm]
-        self.y_position_microns = y * microns  # y(t) [µm]
-        self.xy_points_microns = np.array([self.x_position_microns, self.y_position_microns]).T
+    def __init__(
+        self,
+        x_position,
+        y_position,
+        time_increment_s=1,
+        scale_um=1,
+        window_size=5,
+    ):
+        self.time_increment_s = time_increment_s
+        self.scale_um = scale_um
+        self.window_size = window_size
+        self.num_points = x_position.size
 
+        # scale trajectory from px --> µm
+        self.x_position_um = x_position * self.scale_um  # x(t) [µm]
+        self.y_position_um = y_position * self.scale_um  # y(t) [µm]
+        self.xy_positions_um = np.array([self.x_position_um, self.y_position_um]).T
+
+    def compute_motility_metrics(self):
+        """Compute motility metrics."""
         # calculate point-to-point distances to facilitate motility measurements
-        distances_L1_microns = np.diff(self.xy_points_microns, axis=0)
-        distances_L2_microns = np.linalg.norm(distances_L1_microns, axis=1)
+        distances_L1_um = np.diff(self.xy_positions_um, axis=0)
+        distances_L2_um = np.linalg.norm(distances_L1_um, axis=1)
+        # rolling average for max sprint length
+        distances_L2_um_rolling_average = np.convolve(
+            distances_L2_um, np.ones(self.window_size)
+        ) / self.window_size
 
-        # direction and angle-based calculations
-        x_velocity = np.diff(self.x_position_microns)
-        y_velocity = np.diff(self.y_position_microns)
+        # instantaneous velocities
+        x_velocity = np.diff(self.x_position_um)
+        y_velocity = np.diff(self.y_position_um)
+
+        # instantaneous angle and angular change
         angle = np.arctan2(y_velocity, x_velocity)
-        directional_change = np.diff(angle)
+        angular_change = np.abs(np.diff(angle))
+        # TODO: this feels very wrong but not sure how else to avoid spikes in
+        #       the derivative of the angle when the angle flips from 2π to 0
+        #       (aka from 359° to 0°)
+        curvature = np.where(angular_change - pi > 0, 2 * pi - angular_change, angular_change).sum()
+
+        # directional changes
         x_direction_change = np.abs(np.diff(np.sign(x_velocity))) / 2
         y_direction_change = np.abs(np.diff(np.sign(y_velocity))) / 2
 
         # motility measurements
-        total_time = num_points * seconds
-        total_distance = distances_L2_microns.sum()
-        net_distance = np.linalg.norm(self.xy_points_microns[-1] - self.xy_points_microns[0])
-        max_sprint_length = distances_L2_microns.max()
+        total_time = self.num_points * self.time_increment_s
+        total_distance = distances_L2_um.sum()
+        net_distance = np.linalg.norm(self.xy_positions_um[-1] - self.xy_positions_um[0])
+        max_sprint_length = distances_L2_um_rolling_average.max()
         confinement_ratio = net_distance / total_distance
-        mean_curvilinear_speed = (distances_L2_microns / seconds).mean()
+        mean_curvilinear_speed = total_distance / total_time
         mean_linear_speed = net_distance / total_time
-        mean_angular_velocity = (directional_change / seconds).mean()
-        linearity_of_progression = mean_linear_speed / mean_curvilinear_speed
+        mean_angular_velocity = curvature / total_time
+        num_rotations = curvature / (2 * pi)
         num_direction_changes = x_direction_change.sum() + y_direction_change.sum()
+        pivot_rate = num_direction_changes / total_distance
 
-        self.measurements = {
+        measurements = {
             "total_time": total_time,
             "total_distance": total_distance,
             "net_distance": net_distance,
@@ -230,6 +323,9 @@ class TrajectoryAnalyzer:
             "mean_curvilinear_speed": mean_curvilinear_speed,
             "mean_linear_speed": mean_linear_speed,
             "mean_angular_velocity": mean_angular_velocity,
-            "linearity_of_progression": linearity_of_progression,
+            "num_rotations": num_rotations,
             "num_direction_changes": num_direction_changes,
+            "pivot_rate": pivot_rate,
         }
+
+        return measurements
